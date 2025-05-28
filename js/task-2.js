658536370552674db6580cc753d5caa4793f3090
Mykyta <nik.from.varga@gmail.com>
2025-05-28 18:09:57 +0300

    fix1

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,12 @@
 function makeArray(firstArray, secondArray, maxLength) {
   let compose = firstArray.concat(secondArray);
-  if (compose.length >= maxLength) {
+  if (compose.length > maxLength) {
     return compose.slice(0, maxLength);
+  } else {
+    return compose;
   }
+  //or use this code instead
+  //return compose.length > maxLength ? compose.slice(0, maxLength) : compose;
 }
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
